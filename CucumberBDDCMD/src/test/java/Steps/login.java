27ecbb39eb52fecc2902a748b71ef3efa1b5d444
Pragya <pragyasingh51@gmail.com>
2022-04-05 15:24:44 +0530

    Commit amazone project

---
+++ CucumberBDDCMD/src/test/java/Steps/login.java
@@ -0,0 +1,39 @@
+package Steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class login {
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+
+	}
+
+	@When("user enters valid username and password")
+	public void user_enters_valid_username_and_password() {
+
+	}
+
+	@And("clicks on login button")
+	public void clicks_on_login_button() {
+
+	}
+
+	@Then("user is navigated on home page")
+	public void user_is_navigated_on_home_page() {
+	}
+
+	@When("user enters invalid username and password")
+	public void user_enters_invalid_username_and_password() {
+	}
+
+	@Then("error message dispalyed  - Invalid Credentials")
+	public void error_message_dispalyed_Invalid_Credentials() {
+	}
+
+
+
+
+}
